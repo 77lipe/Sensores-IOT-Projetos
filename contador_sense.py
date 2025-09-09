6eaa1219e3bc5cbe0905f5ced78492d473ec72dd
77lipe <lipevds.13@gmail.com>
2025-09-09 14:53:37 -0300

    up code sensor de objeto funcionando!!

--- contador_sense.py
+++ contador_sense.py
@@ -2,32 +2,63 @@ from machine import Pin, time_pulse_us
 import time
 
 
-PINO_ECHO = 25
-LED = 14
+PINO_TRIG = 27
+PINO_ECHO = 26
+PINO_LED_ITENS = 18  #BRANCO
+PINO_LED_CAIXAS = 15 #VERDE
 
-
+trig = Pin(PINO_TRIG, Pin.OUT)
 echo = Pin(PINO_ECHO, Pin.IN)
-led = Pin(LED, Pin.OUT)
+led = Pin(PINO_LED_ITENS, Pin.OUT)
+led_caixa = Pin(PINO_LED_CAIXAS, Pin.OUT)
 
-contador = 0
-cont_anterior = 0
 
-# LOOP #
+cont_itens = 0
+cont_caixas = 0
+dist_maxima = 10  
+detc = False  
+
+
+def calc_distancia():
+    trig.value(0)
+    time.sleep_us(2)
+
+    trig.value(1)
+    time.sleep_us(10)
+    trig.value(0)
+
+    duracao = time_pulse_us(echo, 1, 300000)
+    if duracao < 0:  
+        return 1000  
+    distancia = (duracao / 2) * 0.0343
+    return distancia
+
 while True:
-    cont_atual = echo.value()
+    dist = calc_distancia()
 
-    #loopin de contagem
-    if cont_atual == 1 and cont_anterior == 0:
-        contador +=1
-        print("Objeto Detectado!", contador)
-        led.value(1)
-        time.sleep(2)
+    if dist <= dist_maxima:
+        led.value(1)  
+
+        if not detc:
+            cont_itens += 1
+            detc = True
+            print("Item detectado! Quantidade:", cont_itens)
+
+           
+            if cont_itens >= 10:
+                cont_caixas += 1
+                cont_itens = 0 
+                print("Caixa completa! Número de caixas:", cont_caixas)
+                led_caixa.value(1)
+                print("Iniciando Próxima Caixa!")
+
+        time.sleep(1)
+                
+
+    else:
         led.value(0)
-    cont_anterior = cont_atual
-    time.sleep(3)
-    elif  cont_atual > 10:
-        contador = 0
-        print("Caixa completa! Nova caixa")
-        time.sleep(5)
-       
-     
+        led_caixa.value(0)
+
+        detc = False
+
+    time.sleep(0.5)
